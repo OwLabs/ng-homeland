99bbf0df80ee14ede73d04d22bea4e3384f5a040
Ahmad Haziq <ahmadhaziq2600@gmail.com>
2026-02-08 01:10:14 +0800

    feat(landing-page): added section B to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import SectionA from "@/components/landing-page/section-a";
+import SectionB from "@/components/landing-page/section-b";
 
 export default function Home() {
   return (
     <div>
       <SectionA />
+      <SectionB />
     </div>
   );
 }
